a2f2d7825afacd879f50709d63794a53acbc24d5
Oswaldo Raúl Carreón Silva <46851288+OswaldoCarreon@users.noreply.github.com>
2020-02-01 20:10:56 -0600

    Create Secante.py

---
+++ Secante.py
@@ -0,0 +1,28 @@
+#Secante
+import math
+
+def f(x):   return math.log(x-1)+ math.cos(x-1)
+
+
+def Secante(f,a,b,tolerance):
+    relative_error = relative_error = abs((b-a)/abs(b))
+    i = 0
+    while(relative_error > tolerance):
+        temp = a
+        a = b
+        b = temp - ( f(temp)*(temp-b) ) / ( f(temp) - f(b))
+        relative_error = relative_error = abs((b-a)/abs(b))
+        i+=1
+        
+        
+    print(i)
+    print(relative_error)
+    print(b)
+        
+    
+
+p0 = 1.3
+p1 = 2
+T = 1e-5
+
+Secante(f,p0,p1,T)
