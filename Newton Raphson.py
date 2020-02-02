9c849a995745ecc535e8256c54b7bb698c475970
Oswaldo Raúl Carreón Silva <46851288+OswaldoCarreon@users.noreply.github.com>
2020-02-01 19:16:47 -0600

    Update Newton Raphson.py

--- Newton Raphson.py
+++ Newton Raphson.py
@@ -1,32 +1,29 @@
 #Newton raphson
 import math
-import sympy as sy
+#import sympy as sy
 from scipy.misc import derivative
 
-
-
 def f(x):   return math.log(x-1)+ math.cos(x-1)
+def g(x):   return (x-2)**2-math.log(x)
+def h(x):   return 2*x*math.cos(2*x)-(x-2)**2
 
 def Newton_Raphson(f,pn,tolerance):
     i = 0
     pn_1 = pn #Punto anterior
     pn = pn_1 - (f(pn_1) / derivative(f,pn_1,dx=1e-6) ) #Punto nuevo
-    relative_error = (pn-pn_1)/(abs(pn))
+    relative_error = abs((pn-pn_1)/(abs(pn)))
+    print("Primera vuelta", pn,pn_1)
     
-    while(relative_error < tolerance):
+    #escribir en excel
+    while(relative_error > tolerance):
         pn_1 = pn
         pn = pn_1 - (f(pn_1) / derivative(f,pn_1,dx=1e-6) ) #Punto nuevo
         relative_error = (pn-pn_1)/(abs(pn))
         i+=1
         
-    print(relative_error)
-    print(pn)
-    print(i)
-    
-    
-x = 1.6
-T = 1e-1
-Newton_Raphson(f,x,T)
+x = 1
+T = 1e-5
+Newton_Raphson(g,x,T)
 
 
 
