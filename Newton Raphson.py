fdff19f02e25883525da6a8d11c8ae834d3f3347
Oswaldo Raúl Carreón Silva <46851288+OswaldoCarreon@users.noreply.github.com>
2020-02-01 20:14:49 -0600

    Update Newton Raphson.py
    Terminando el método de Newton

--- Newton Raphson.py
+++ Newton Raphson.py
@@ -12,18 +12,19 @@ def Newton_Raphson(f,pn,tolerance):
     pn_1 = pn #Punto anterior
     pn = pn_1 - (f(pn_1) / derivative(f,pn_1,dx=1e-6) ) #Punto nuevo
     relative_error = abs((pn-pn_1)/(abs(pn)))
-    print("Primera vuelta", pn,pn_1)
-    
+
     #escribir en excel
     while(relative_error > tolerance):
         pn_1 = pn
         pn = pn_1 - (f(pn_1) / derivative(f,pn_1,dx=1e-6) ) #Punto nuevo
         relative_error = (pn-pn_1)/(abs(pn))
         i+=1
+    return pn
         
 x = 1
 T = 1e-5
-Newton_Raphson(g,x,T)
+raiz = Newton_Raphson(g,x,T)
+print(raiz)
 
 
 
